6ac2d020c45053ba65b376ded348e357545e48b8
mashamaslennikova <mashamaslennikova04@gmail.com>
2022-12-26 19:36:14 -0800

    Create 4.cs

---
+++ 4.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+interface IGeographicObject
+{
+    int X
+    { set; get; }
+    int Y
+    { set; get; }
+    public string Name
+    { set; get; }
+    public void WayToGetInfo()
+    {
+
+    }
+}
+
+class River : IGeographicObject
+{
+    public River(int x, int y, string name, string description)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+        Description = description;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    void IGeographicObject.WayToGetInfo()
+    {
+
+    }
+
+    private double FlowRate;
+    private double Length;
+}
+
+class Mountain : IGeographicObject
+{
+    public Mountain(int x, int y, string name, string description)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+        Description = description;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    void IGeographicObject.WayToGetInfo()
+    {
+
+    }
+    private double HighestPoint;
+}
+
